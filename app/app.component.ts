52eab1586821edca492d976fe4ade6ae82c598bf
Sebastian <scastaldi@gmail.com>
2016-03-23 19:02:53 -0400

    saving services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,27 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/styles.css'],
-    directives: [HeroDetailComponent]    
+    directives: [HeroDetailComponent],
+    providers: [HeroService]    
 })
 export class AppComponent {
     title = 'Tour of Heroes';
     selectedHero : Hero;    
-    heroes = HEROES;
+    heroes: Hero[];
     onSelect(hero: Hero) { this.selectedHero = hero; }
+    constructor(private _heroService: HeroService) {}
+    getHeroes(){
+        this.heroes = this._heroService.getHeroes();
+    }
+    
+    ngOnInit(){
+        this.getHeroes();
+    }
 }
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
 
